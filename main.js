36394d561ba1f6442c35e25456d5a4f9acce8dcd
caioERP <caiorabelo10@gmail.com>
2022-11-30 11:19:35 -0300

    initial commit

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-const character = document.getElementsByClassName("character")[0];
-const containerCharacter = document.getElementsByClassName("container-character")[0];
+const character = document.getElementsByClassName("character")[0];          /* utilizado para pegar um elemnto do html, e transformar em um objeto */
+const containerCharacter = document.getElementsByClassName("container-character")[0];      /* utilizado para pegar um elemnto do html, e transformar em um objeto */
 
 
-const VELOCITY = 10;
+const VELOCITY = 10; /* aqui é definida a velocidade do personagem */
 
-const SCREEN_WIDTH = screen.width;
-const SCREEN_HEIGHT = screen.height;
+const SCREEN_WIDTH = screen.width;   /* aqui é definido o tamanho da tela */
+const SCREEN_HEIGHT = screen.height;  /* aqui é definido o tamanho da tela */
 
-let xPosition = 500;
-let yPosition = 300;
 
-const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
-const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
+let xPosition = 500;   /* aqui é definido o posicionamento inicial do personagem no eixo x */
+let yPosition = 300;   /* aqui é definido o posicionamento inicial do personagem no eixo y */
 
-window.addEventListener("keydown", (event) => {
+const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]   /* aqui são definidas as variaveis para as teclas */
+const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];     /* e aqui as variaveis para os movimentos */
+
+window.addEventListener("keydown", (event) => {                 /* essa função é utilizada para reconhecer o evento de quando o usuario usa a seta */
     const key  = event.key;
 
     const keyPressedAvaiable =  keysAvaiable.some((currentKey) => {
@@ -27,26 +28,26 @@ window.addEventListener("keydown", (event) => {
     })
 
 
-    if(key === "ArrowUp"){
+    if(key === "ArrowUp" && yPosition > 0){          /* aqui cada if serve para dizer a direção que o personagem deve se mover */
         character.classList.add("turnUp");
         yPosition -= VELOCITY;
     }
 
-    if(key === "ArrowDown"){
+    if(key === "ArrowDown" && yPosition < SCREEN_HEIGHT-75){
         character.classList.add("turnDown");
         yPosition += VELOCITY;
     }
 
-    if(key === "ArrowLeft"){
+    if(key === "ArrowLeft" && xPosition > 0 ){
         character.classList.add("turnLeft");
         xPosition -= VELOCITY;
     }
 
-    if(key === "ArrowRight"){
+    if(key === "ArrowRight" && xPosition < 1300){
         character.classList.add("turnRight");
         xPosition += VELOCITY;
     }
 
-    containerCharacter.style.top = `${yPosition}px`;
-    containerCharacter.style.left = `${xPosition}px`
+    containerCharacter.style.top = `${yPosition}px`;     /* integração com css para definir onde está o personagem */
+    containerCharacter.style.left = `${xPosition}px`     /* integração com css para definir onde está o personagem */
 });
